b82280c3a79f1670c18fb48f8c7c5c24124ddd91
bailey halliday <you@example.com>
2024-08-30 18:13:30 +0100

    tested another configuration

--- Khet/Khet.Wpf/ViewModels/MainViewModel.cs
+++ Khet/Khet.Wpf/ViewModels/MainViewModel.cs
@@ -18,10 +18,15 @@ namespace Khet.Wpf.ViewModels
 
 
             squareViewModels[30].activePiece = new PieceViewModel() { state = State.dl };
-            squareViewModels[35].activePiece = new PieceViewModel() { state = State.dl };
+            squareViewModels[33].activePiece = new PieceViewModel() { state = State.dl };
+
+
+            squareViewModels[49].activePiece = new PieceViewModel() { state = State.dl };
+            squareViewModels[45].activePiece = new PieceViewModel() { state = State.dl };
+            
 
             squareViewModels[0].FireLaser(Direction.down);
-
+            squareViewModels[squareViewModels.Count-1].FireLaser(Direction.up);
 
         }
     }
