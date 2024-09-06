96bb01c8b2d4887fbcc76b6ddf704702fe008e9f
bailey halliday <you@example.com>
2024-09-06 21:11:31 +0100

    Begun refactor. Ran into bizzare behaviour - A Left click command bound to the board view was not firing unless a square had a piece in it. The fix to that was setting the sqaure view background to transparent to trick wpf into having it be a hittable object

--- Khet/Khet.Wpf/ViewModels/SquareViewModel.cs
+++ Khet/Khet.Wpf/ViewModels/SquareViewModel.cs
@@ -13,10 +13,6 @@ namespace Khet.Wpf.ViewModels
 {
     public class SquareViewModel : ViewModelBase
     {
-        public SquareViewModel? upNeighbour {  get; set; }
-        public SquareViewModel? downNeighbour {  get; set; }
-        public SquareViewModel? leftNeighbour {  get; set; }
-        public SquareViewModel? rightNeighbour {  get; set; }
 
         private IPiece _activePiece;
         public IPiece activePiece { get => _activePiece; set => SetProperty(ref _activePiece, value); }
@@ -30,46 +26,17 @@ namespace Khet.Wpf.ViewModels
         }
 
 
-        public void FireLaser(Direction firingDirection)
+        public Direction FireLaser(Direction firingDirection)
         {
 
             var inDirection = getInDirection(firingDirection);
-            Direction? outDirection = firingDirection;
+            Direction outDirection = firingDirection;
 
             // Display this sqaures laser
             activeLaser = new LaserBeamViewModel(inDirection, activePiece);
+          
 
-            
-
-            if (activePiece != null)
-            {
-               outDirection = activePiece?.ResolveLaserDirection(inDirection);
-                
-            }
-
-
-
-            switch (outDirection)
-            {
-                case Direction.down:
-                    downNeighbour?.FireLaser(Direction.down);
-                    break;
-
-                case Direction.up:
-                    upNeighbour?.FireLaser(Direction.up);
-                    break;
-
-                case Direction.left:
-                    leftNeighbour?.FireLaser(Direction.left);
-                    break;
-
-                case Direction.right:
-                    rightNeighbour?.FireLaser(Direction.right);
-                    break;
-                case Direction.kill:
-                    activePiece = null;
-                    break;
-            }
+            return outDirection;
         }
 
         private Direction getInDirection(Direction firingDirection)
