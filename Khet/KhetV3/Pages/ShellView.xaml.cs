02be07bf6b1055c3fb414935f3ecb0fb0bc7c7b4
bailey halliday <you@example.com>
2024-10-09 18:40:50 +0100

    Added custom tool bar at top

--- Khet/KhetV3/Pages/ShellView.xaml.cs
+++ Khet/KhetV3/Pages/ShellView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace KhetV3.Pages
 {
@@ -10,6 +11,36 @@ namespace KhetV3.Pages
         public ShellView()
         {
             this.InitializeComponent();
+        }
+
+        // Allow the window to be moved by dragging the custom title bar
+        private void CustomTitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        // Minimize window
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        // Maximize or restore window
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        // Close window
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
